d0b174511180843d5bf2f569324037831d8864dd
Sam Corey <112501671+secorey@users.noreply.github.com>
2024-04-01 09:24:23 -0500

    Add files via upload
    updated data cleaning file

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,93 +1,122 @@
 # This script takes in the original raw data files and generates two files.
 # The two files are as follows:
-# 1) Combined .csv file of all the original raw files across all years.
+# 1) Combined .csv file of all the original raw files across all years. Column
+#    added:
+#    - vol_attrition_next_year: whether the employee quit in the following year
 # 2) .csv file for survival analysis. This will be the earliest file (2018)
-#    with new hires from following years added. Two more columns will be added:
-#    attrition y/n and number of years (either until attrition or until end of
-#    analysis).
+#    with new hires from following years added. Four more columns will be added:
+#    - year: year of the earliest file where the employee shows up. This is
+#      either the year they were hired or 2018
+#    - vol_attrition (1/0): whether the employee quit or retired
+#    - attrition (1/0): whether the employee turned over (either voluntarily or
+#      involuntarily)
+#    - num_years: number of years until attrition or until the end of analysis
+# Notes:
+# - Filter out cases where there is a termination date but no term reason (might
+#   be next-day rehires)?
 
-library(tidyverse)
+# options(warn = -1)
 
-df_2021 <- read_csv("data/sample/sample_data_2021.csv")
-df_2022 <- read_csv("data/sample/sample_data_2022.csv")
-df_2023 <- read_csv("data/sample/sample_data_2023.csv")
+suppressMessages({
+  library(tidyverse)
+  library(readxl)
+})
+
+read_file <- function(file_path) {
+  # This function reads in the raw .xlsx file and applies filters that will
+  # be necessary for both the one-year predictions and survival analysis
+  suppressMessages({
+    df <- read_excel(file_path)
+  })
+  # Remove top rows and change column names:
+  colnames(df) <- unlist(df[4, ])
+  df <- df[-(1:4), ]
+  
+  df <- df %>%
+    filter(is.na(`Next day rehire`),
+           `Employment Status` == "Regular",
+           Country != "Korea, Republic of",
+           (`Termination Reason` != "Voluntary: Retirement") | 
+             (is.na(`Termination Reason`))) %>%
+    mutate(`As of Date` = 
+             as.Date(as.numeric(`As of Date`), origin = "1899-12-30"),
+           `Termination Date` = 
+             as.Date(as.numeric(`Termination Date`), origin = "1899-12-30"),
+           year = year(`As of Date`),
+           vol_attrition = 
+             ifelse(!(`Termination Category` == 
+                        "Terminate Employee > Voluntary") |
+                      (is.na(`Termination Category`)), 0, 1),
+           attrition = ifelse(is.na(`Termination Category`), 0, 1),
+           Tenure = as.numeric(ifelse(Tenure < 0, NA, Tenure)))
+  return(df)
+}
+
+df_2018 <- read_file("data/snapshots/EE Snapshot 2018.12.31.xlsx")
+df_2019 <- read_file("data/snapshots/EE Snapshot 2019.12.31.xlsx")
+df_2020 <- read_file("data/snapshots/EE Snapshot 2020.12.31.xlsx")
+df_2021 <- read_file("data/snapshots/EE Snapshot 2021.12.31.xlsx")
+df_2022 <- read_file("data/snapshots/EE Snapshot 2022.12.31.xlsx")
+df_2023 <- read_file("data/snapshots/EE Snapshot 2023.12.31.xlsx")
 
 # Combine data frames into one:
-df <- rbind(df_2021, df_2022, df_2023)
+df <- rbind(df_2018, df_2019, df_2020, df_2021, df_2022, df_2023)
 
-# Category name for voluntary turnover:
-quit_cat_name <- "Terminate Employee > Involuntary" # Change this #######
-
-# Create columns:
-# Year of data collection:
-df$year <- substr(df$`As of Date`,
-                  nchar(df$`As of Date`) - 3,
-                  nchar(df$`As of Date`))
-# Whether the employee turned over in that year:
-df$attrition <- ifelse(df$`Termination Category` == quit_cat_name, 1, 0)
-df$attrition[is.na(df$attrition)] <- 0
-
-# Filter out irrelevant units (retirements):
-# df <- df %>%
-#   filter((`Termination Reason` !=
-#            "Involuntary: Retirement/Resignation with Package") | 
-#            is.na(`Termination Reason`))
+# Add a variable that indicates if an employee quit in the following year:
+df <- df %>% 
+  arrange(`Employee ID`, year) %>%
+  group_by(`Employee ID`) %>%
+  mutate(vol_attrition_next_year = lead(vol_attrition)) %>%
+  ungroup()
 
 # Export first file to CSV:
-write_csv(df, "data/sample/sample_agg.csv")
+write_csv(df, "data/agg_one_year.csv")
+
 
 # Create second file:
 # For each iteration, in the old file, add 1 year to everyone who has not 
 # attrited. Then, in the new file, check for new employee IDs and add those rows 
 # with 0 for `num_years`. Then, check for all quits in the new file and change 
-# those employees to 1 for `attrition`.
+# those employees to 1 for `attrition` and for `vol_attrition`. Involuntary
+# turnovers receive a 1 for `attrition` and a 0 for `vol_attrition`.
 
-df_for_survival <- df_2021
-
-df_for_survival <- mutate(df_for_survival, 
-                          attrition = ifelse(
-                            (df_for_survival$`Termination Category` != 
-                               quit_cat_name) | 
-                              is.na(`Termination Category`), 
-                            0, 1),
-                          num_years = 0)
-for (df_year in list(df_2022, df_2023)) {
+df_for_survival <- mutate(df_2018, num_years = 0)
+for (df_year in list(df_2019, df_2020, df_2021, df_2022, df_2023)) {
   # Add one year to non-attrited employees:
-  df_for_survival$num_years[df_for_survival$attrition == 0] <- 
+  df_for_survival$num_years[df_for_survival$attrition == 0] <-
     df_for_survival$num_years[df_for_survival$attrition == 0] + 1
   # Add new employees from df_year to df_for_survival:
-  new_employees <- 
-    filter(df_year, 
+  new_employees <-
+    filter(df_year,
            !(`Employee ID` %in% df_for_survival$`Employee ID`)) %>%
-    mutate(attrition = 0, num_years = 0)
+    mutate(num_years = 0)
   df_for_survival <- rbind(df_for_survival, new_employees)
-  # Find employees who quit in df_year and 
-  # change attrition status in df_for_survival:
-  quit_employees <- df_year %>%
-    filter(`Termination Category` == quit_cat_name) %>%
-    select(`Employee ID`, 
-           `Termination Date`, 
-           `Termination Category`, 
-           `Termination Reason`)
-  df_for_survival$attrition[
-    df_for_surival$`Employee ID` %in% quit_employees$`Employee ID`] <- 1
-  df_for_survival$`Active Status`[
-    df_for_surival$`Employee ID` %in% quit_employees$`Employee ID`] <- NA
-  # Change term date, term category, and term reason for quit employees:
-  df_for_survival$`Termination Date`[
-    df_for_surival$`Employee ID` %in% quit_employees$`Employee ID`] <- 
-    quit_employees$`Termination Date`
-  df_for_survival$`Termination Category`[
-    df_for_surival$`Employee ID` %in% quit_employees$`Employee ID`] <- 
-    quit_employees$`Termination Category`
-  df_for_survival$`Termination Reason`[
-    df_for_surival$`Employee ID` %in% quit_employees$`Employee ID`] <- 
-    quit_employees$`Termination Reason`
+  # Find employees who attrited/quit in df_year:
+  attrited_employees <- df_year %>%
+    filter(attrition == 1) %>%
+    select(`Employee ID`,
+           `Termination Date`,
+           `Termination Category`,
+           `Termination Reason`,
+           attrition,
+           vol_attrition)
+  # Overwrite termination information in df_for_survival:
+  for (feature in c("Termination Date", "Termination Category", 
+                    "Termination Reason", "attrition", "vol_attrition")) {
+    df_for_survival[[feature]][
+      df_for_survival$`Employee ID` %in% attrited_employees$`Employee ID`] <-
+      attrited_employees[[feature]]
+  }
   # `As of Date` now becomes the first date of observation of this employee.
   # This is either the year they are hired or the first year of observation.
   # All other variables are also from this first date.
 }
 
-write_csv(df_for_survival, "data/sample/sample_survival_agg.csv")
+# Change active status for attrited employees:
+df_for_survival$`Active Status` <- ifelse(df_for_survival$attrition == 0, 
+                                          "Yes", 
+                                          NA)
 
+write_csv(df_for_survival, "data/agg_survival.csv")
 
+# options(warn = 0)
